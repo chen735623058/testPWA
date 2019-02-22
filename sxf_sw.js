684520c8fb782ad8f25b982978a584e3e170a8e8
sunxiaofan <sunxiaofan@kingsoft.com>
2019-02-22 11:57:27 +0800

    增加了 推送订阅的方法

--- sxf_sw.js
+++ sxf_sw.js
@@ -49,3 +49,16 @@ this.addEventListener('fetch', function(event)  {
         })
     );
 });
+
+// 监听推送的消息
+self.addEventListener('push', function (e) {
+    var data = e.data;
+    if (e.data) {
+        data = data.json();
+        console.log('push的数据为：', data);
+        self.registration.showNotification(data.text);
+    }
+    else {
+        console.log('push没有任何数据');
+    }
+});
